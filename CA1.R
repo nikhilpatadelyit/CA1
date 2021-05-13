ae32a2839cc94b61b735e7edbda5956ab3490443
unknown <l00157070@student.lyit.ie>
2021-05-13 01:19:44 +0100

    Initial Commit TO CA1

--- CA1.R
+++ CA1.R
@@ -80,8 +80,8 @@ pairs.panels(new_heart_data,
 
 # Question 1:
 # Peoples at different ages can get a heart-diseases
-# H0 = Chance of getting a heart-disease between the age(29 to 77)
-# H1 = Chance of not getting a heart-disease between the age(29 to 77)
+# H0 = More chance of getting a heart-disease between the age(29 to 77)
+# H1 = Less chance of getting a heart-disease between the age(29 to 77)
 
 # Convert the Target variable to
 # a categorical dichotomous variable with appropriate labels
@@ -134,6 +134,7 @@ qqnorm(Age)
 # Add a line that represents the ND
 qqline(Age, col = "red")
 
+# Comparing the two variables
 with(new_heart_data, 
      qqplot(Age[Target == "More chance of getting HA"], 
             Age[Target == "Less chance of getting HA"], 
@@ -171,7 +172,8 @@ wilcox.test(Age~Target)
 # the chance of patient getting HA between the age(29 to 77) is less
 
 # Answer for Question 1:
-# Thus the chance of patient getting a HA between the age(29 to 77) is more.
+# Thus the chance of patient getting a HA 
+# between the age(29 to 77) is more.
 
 
 
@@ -245,14 +247,15 @@ chisq$p.value
 # p-value < 0.05 so this indicates that the
 # Null (H0) hypothesis is rejected
 # therefore this indicates that
-# the chance of female patient getting HA is less compared to male
+# the chance of male patient getting HA is less compared to female
 
 # Answer to Question 2:
-# Thus the chance of male patient getting HA is more with th female.
+# Thus the chance of male patient getting HA is more then with female.
 
 
 # Question 3:
-# Regular exercise of may reduced to the chest pain and can have less chances of HA
+# Regular exercise of may reduced to the chest pain and 
+# can have less chances of HA
 # H0 = Exercise will reduced the chest pain & less chance of HA
 # H1 = Exercise will not reduced the chest pain & can get HA
 
@@ -334,17 +337,18 @@ chisq$p.value
 # p-value < 0.05 so this indicates that the
 # Null (H0) hypothesis is rejected
 # therefore this indicates that
-# the daily exercise is required to reduce the chest pain and lower the risk of HA
+# the daily exercise will not reduce the chest pain and
+# higher the chance of HA
 
 # Answer to Question 3:
-# Thus the chance of getting a high risk of chest pain which may lead to HA
-# is more if daily exercise is not concerned
+# Thus the daily exercise will reduced the chest pain and
+# less chance of HA
 
 # Question 4:
 # Cholesterol level can block/damage the blood vessel which can lead to HA
 # the cholesterol levels are correlated with blood arteries
-# H0 = Cholesterol level has an effect on arteries
-# H1 = Cholesterol has no effect on arteries
+# H0 = Cholesterol level has an effect on blood arteries
+# H1 = Cholesterol level has no effect on blood arteries
 
 # Analyze the data with specified attributes
 table(new_heart_data$Num_major_vessel)
@@ -352,6 +356,25 @@ table(new_heart_data$Cholestoral)
 
 # Comparing the analysis of the specified attributes
 table(new_heart_data$Num_major_vessel, new_heart_data$Cholestoral)
+
+# Convert the Num_major_vessel variable to
+# a categorical nominal variable with appropriate levels
+new_heart_data$Num_major_vessel <- as.factor(new_heart_data$Num_major_vessel)
+
+# Renaming the levels of the Chest Pain as
+# 0 = No Blocks
+# 1 = 1 Block
+# 2 = 2 Block
+# 3 = 3 Block
+# 4 = 4 Block
+levels(new_heart_data$Num_major_vessel) = c("No Blocks", 
+                                            "1 Block", 
+                                            "2 Blocks", 
+                                            "3 Blocks", 
+                                            "4 Blocks")
+
+# Show the vessel levels
+levels(new_heart_data$Num_major_vessel)
 
 # Plot the graph to analyze the specified attributes
 plot(Cholestoral, Num_major_vessel, pch = 9, col = 'lightblue', 
@@ -379,61 +402,178 @@ qqnorm(Num_major_vessel)
 qqline(Num_major_vessel, col = "red")
 # The blood vessel data is not ND
 
-
-# Install package and import the library for "ggpubr"
-#install.packages("ggpubr")
-#library("ggpubr")
-
-# Here we will be using the correlation coefficient test
-# Compute the correlation of the required attributes using cor()
-# We can use any of the methods specified below
-cor(new_heart_data$Cholestoral, new_heart_data$Num_major_vessel, 
-    method = c("pearson", "kendall", "spearman"))
-
-# It test for the correlation and returns the value for
-# correlation coefficient and p-value
-# we can use any of the method specified below
-cor.test(new_heart_data$Cholestoral, new_heart_data$Num_major_vessel, 
-         method=c("pearson", "kendall", "spearman"))
-
-# We are using pearson test for hypothesis
-cor(new_heart_data$Cholestoral, new_heart_data$Num_major_vessel,  
-    method = "pearson", use = "complete.obs")
-
-# Checking the test assumption with the Shapiro-Wilk test
-# Shapiro-Wilk normality test for Cholesterol
-shapiro.test(new_heart_data$Cholestoral)
-# p -value = 5.365e-09 = 0.00000002
-
-# Shapiro-Wilk normality test for arteries
-shapiro.test(new_heart_data$Num_major_vessel)
-# p -value = 2.2e-16 = 0.00000003
-
-# Correlation test for the variable
-kendal_test <- cor.test(new_heart_data$Cholestoral, new_heart_data$Num_major_vessel, 
-                method = "kendall")
-kendal_test
-
-# Extract the p.value
-kendal_test$p.value
-# The p-value is = 0.04
-
-# Extract the correlation coefficient
-# The kendall correlation coefficient is (tau)
-kendal_test$estimate
-# tau = 0.088
-
-# cut-off = 0.054
+# As we have one continuous and other categorical we will be using
+# Kruskal-Wallis test for the better computation of the data
+kruskal.test(Cholestoral ~ Num_major_vessel, data = new_heart_data)
 # p-value = 0.04
-# p-value < 0.05 = Reject H0
 
+# function pairwise.wilcox.test() to calculate pairwise comparisons between
+# arteries levels
+pairwise.wilcox.test(new_heart_data$Cholestoral, new_heart_data$Num_major_vessel, 
+                     p.adjust.method = "BH")
+
+# The pairwise comparison shows that the levels are significantly different
+
+# p-value is = 0.04 which is less than the cut-off = 0.05
+# Thus the result interprets that there is significant differences between
+# the blood vessels
+
+# p-value < 0.05 = Reject H0
 # p-value < 0.05 so this indicates the 
 # Null (H0) hypothesis is rejected
 # therefore this indicates that
 # Cholesterol level has no significance effect with the blood arteries
 
 # Answer to Question 4:
-# Thus the cholesterol level affects major arteries & are significantly 
-# correlated with a correlation coefficient.
+# Thus the cholesterol level affects major arteries & are significantly different
+
+# Install package and import the library for "tidyverse"
+# install.packages("tidyverse")
+library(tidyverse)
+
+# Install package and import the library for "ggpubr"
+# install.packages("ggpubr")
+library(ggpubr)
+
+# Question 5:
+# Patients sugar level can cause the heart disease which can
+# further lead to cause the heart attack
+# (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)
+# H0 = Increase in sugar level can cause heart failure
+# H1 = Increase in sugar level cannot led to heart failure
+
+# Analyze the data with specified attributes
+table(new_heart_data$Fasting_BS)
+table(new_heart_data$Target)
+
+# Comparing the analysis of the specified attributes
+table(new_heart_data$Fasting_BS, new_heart_data$Target)
+
+# Convert the Exercise_Angina variable to
+# a categorical dichotomous variable with appropriate labels
+# (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)
+new_heart_data$Fasting_BS <- factor(new_heart_data$Fasting_BS, 
+                                          labels = c("False", "True"))
+
+# Structure of the DF after the conversion
+str(new_heart_data)
+
+# Installing and importing library "gplots"
+# install.packages("gplots")
+library(gplots)
+
+# Visualizing the data
+balloonplot(t(Q5), main = "Comparison of the Sugar Level v/s Heart Attack", 
+            xlab = "Sugar Level", ylab = "Target", 
+            label = FALSE, show.margins = FALSE)
+
+# Analysing the distribution of the variables
+histogram(~Fasting_BS | Target, 
+          data = new_heart_data, 
+          main = "Distribution of Sugar Level v/s Heart Attack", 
+          xlab = "Sugar Level Status(mg/dl)", ylab = "Count of people getting HA") 
+
+
+# Applying the chi-square statistic with the function
+# it can be applied as both are the categorical variables
+chisq <- chisq.test(new_heart_data$Fasting_BS, new_heart_data$Target)
+chisq
+# p-value = 0.74
+
+# Observed count values for the hypothesis
+chisq$observed
+
+# Expected count of the values for the hypothesis
+round(chisq$expected)
+
+
+# Print the p.value
+chisq$p.value
+
+# cut-off = 0.05
+# p-value = 0.74
+# p-value > 0.05 then we, Accept H0
+
+# p-value > 0.05 so this indicates that the
+# Null (H0) hypothesis is accepted
+# therefore this indicates that
+# the increase in sugar level can cause the chances of heart attack
+
+# Answer to Question 5:
+# Thus the chance of getting heart attack is less if the sugar level is not increased
+
+# Question 6:
+# Blood pressure level can also cause the heart disease which may
+# lead to heart attack
+# normal range of BP = 120
+# abnormal range of BP > 120
+# H0 = Rise in blood pressure can cause HA
+# H1 = Rise in blood pressure cannot cause HA
+
+# Analyze the data with specified attributes
+table(new_heart_data$Resting_BP)
+table(new_heart_data$Target)
+
+# Comparing the analysis of the specified attributes
+table(new_heart_data$Resting_BP, new_heart_data$Target)
+
+# Plot the graph to analyze the specified attributes
+plot(Resting_BP, Target, pch = 9, col = "LightBlue")
+
+# Analyzing the distribution of the variables
+histogram(~Resting_BP | Target, 
+          data = new_heart_data, 
+          main = "Distribution of Blood Pressure v/s Heart Attack", 
+          xlab = "Blood Pressure(mm Hg)", ylab = "Count of people getting HA")
+
+# Quantile-quantile plot (Q-Q-Plot) allows us to check
+# if the data is ND or not
+qqnorm(Resting_BP)
+
+# Add line that represent the ND
+qqline(Resting_BP, col = "red")
+# It is ND
+
+# Comparing the two variables
+with(new_heart_data, 
+     qqplot(Resting_BP[Target == "More chance of getting HA"], 
+            Resting_BP[Target == "Less chance of getting HA"], 
+            main = "Comparing 2 samples of Target Heart Data", 
+            xlab = "High Pressure Target = More chance of HA", 
+            ylab = "Low Pressure Target = Less chance of HA"))
+
+# Formal test of normality
+# Shapiro-Wilks Test
+# p-value tells us the chances that the sample comes from a ND
+# If p.value > 0.05 then it is normally distributed
+normality_test <- shapiro.test(new_heart_data$Resting_BP)
+normality_test$p.value
+# p.value =1.45e-06 = 0.10
+
+# This test does not work on a dichotomous variable
+with(new_heart_data, tapply(Resting_BP, Target, shapiro.test))
+
+# Results show
+# Less chance of HA with low pressure = p-value = 0.00002 - then it is ND
+# More chance of HA with high pressure = p-value = 0.011 - then it is ND
+
+# After consulting the chart, I am aiming
+# for a dependent var(Blood Pressure)
+# with a independent categorical var(Target)
+# Format for the test is: wilcox.test(dependent var ~ independent var)
+wilcox.test(Resting_BP~Target)
+# p-value = 0.03
+
+# cut-off = 0.05
+# p-value < 0.05 then we, Reject the H0
+
+# p-value < 0.05 so this indicates that the
+# Null (H0) hypothesis is rejected
+# therefore this indicates that
+# the rise in blood pressure cannot cause HA
+
+# Answer for Question 6:
+# Thus higher the blood pressure high are the chances of getting HA
+
 
 
